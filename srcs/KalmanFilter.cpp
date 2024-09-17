bc5e6142af2b9377a64b1bc84db26454b993df2e
Peer De bakker <pde-bakk@f1r1s9.codam.nl>
2024-09-17 18:43:36 +0200

    fixed compile issues

--- srcs/KalmanFilter.cpp
+++ srcs/KalmanFilter.cpp
@@ -5,11 +5,9 @@
 #include "KalmanFilter.hpp"
 
 KalmanFilter::KalmanFilter(const Data& data) {
+	this->state = data.get_position().vstack(data.get_direction()).vstack(data.get_acceleration());
 
-	this->state = data.get_position().vstack(data.get_direction())/*.vstack(data.get_acceleration())*/;
-
-
-	this->process_noise_covariance = Matrix<double, n, n>({
+	this->process_noise_covariance_matrix = Matrix<double, n, n>({
 		std::array<double, n>({pow(GPS_NOISE, 2), 0, 0, 0, 0, 0}),
 		std::array<double, n>({0, pow(GPS_NOISE, 2), 0, 0, 0, 0}),
 		std::array<double, n>({0, 0, pow(GPS_NOISE, 2), 0, 0, 0}),
@@ -17,15 +15,13 @@ KalmanFilter::KalmanFilter(const Data& data) {
 		std::array<double, n>({0, 0, 0, 0, (pow(GYROSCOPE_NOISE, 2) + pow(ACCELEROMETER_NOISE, 2) * DT), 0}),
 		std::array<double, n>({0, 0, 0, 0, 0, (pow(GYROSCOPE_NOISE, 2) + pow(ACCELEROMETER_NOISE, 2) * DT)})
 	});
-
-
 }
 
 
-Vector<double, n> get_initial_process_noise() {
-	const Vector3d gps_noise({0.1, 0.1, 0.1});
-	const Vector3d acceleration_noise({0.001, 0.001, 0.001});
-	const Vector3d gyroscope_noise({0.01, 0.01, 0.01});
+Matrix<double, n, 1> get_initial_process_noise() {
+	const Vector3d gps_noise(std::array<double, 3>({0.1, 0.1, 0.1}));
+	const Vector3d acceleration_noise(std::array<double, 3>({0.001, 0.001, 0.001}));
+	const Vector3d gyroscope_noise(std::array<double, 3>({0.01, 0.01, 0.01}));
 
 	auto x = gps_noise.vstack(acceleration_noise).vstack(gyroscope_noise);
 	return (x);
@@ -48,4 +44,16 @@ Vector3d KalmanFilter::predict(double time_step, const Vector3d& acceleration) {
 //	x = x0 + vx0 * Δt + 1/2 * ax * Δt^2
 //	y = y0 + vy0 * Δt + 1/2 * ay * Δt^2
 //	z = z0 + vz0 * Δt + 1/2 * az * Δt^2
+}
+
+
+Matrix<double, n, n> KalmanFilter::get_state_transition_matrix(double time_step) {
+	auto mat = Matrix<double, n, n>::identity<n>();
+	for (size_t i = 0; i < 6; i++) {
+		mat[i][i + 3] = time_step;
+	}
+	for (size_t i = 0; i < 3; i++) {
+		mat[i][i + 6] = 0.5 * time_step * time_step;
+	}
+	return (mat);
 }
