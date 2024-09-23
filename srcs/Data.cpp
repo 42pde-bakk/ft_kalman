a5689387bf388180261b0c4b69deed9cf0dd6caf
Alpha1337k <oscar@kruithof.io>
2024-09-23 03:36:54 +0200

    Feat: using correct formula for velocity

--- srcs/Data.cpp
+++ srcs/Data.cpp
@@ -45,36 +45,18 @@ double Data::get_acceleration(size_t row_nb, size_t col_nb) const {
 	return (this->acceleration[row_nb][col_nb]);
 }
 
+#include <iostream>
 Vector3d Data::calculate_velocity() const {
-	Vector3d velocity(std::array<double, 3>({0, 0, 0}));
-	double roll = this->direction[0][0];
+	auto speed_m_s = this->speed / 3.6;
+	// double roll = this->direction[0][0];
 	double pitch = this->direction[1][0];
 	double yaw = this->direction[2][0];
 
-	Matrix<double, 3, 3>	r_x({
-		std::array<double, 3>({1, 0, 0}),
-		std::array<double, 3>({0, std::cos(roll), -1 * std::sin(roll)}),
-		std::array<double, 3>({0, std::sin(roll), std::cos(roll)})
-	});
-	Matrix<double, 3, 3>	r_y({
-		std::array<double, 3>({std::cos(pitch), 0, std::sin(pitch)}),
-		std::array<double, 3>({0, 1, 0}),
-		std::array<double, 3>({-1 * std::sin(pitch), 0, std::cos(pitch)})
-	});
-	Matrix<double, 3, 3>	r_z({
-		std::array<double, 3>({std::cos(yaw), -1 * std::sin(yaw), 0}),
-		std::array<double, 3>({std::sin(yaw), std::cos(yaw), 0}),
-		std::array<double, 3>({0, 0, 1})
-	});
-	Matrix<double, 3, 3>	r = r_z * r_y * r_x;
-
-	double	a_x = r[0][0] * this->get_acceleration(0, 0) + r[0][1] * this->get_acceleration(1, 0) + r[0][2] * this->get_acceleration(2, 0);
-	double	a_y = r[1][0] * this->get_acceleration(0, 0) + r[1][1] * this->get_acceleration(1, 0) + r[1][2] * this->get_acceleration(2, 0);
-	double	a_z = r[2][0] * this->get_acceleration(0, 0) + r[2][1] * this->get_acceleration(1, 0) + r[2][2] * this->get_acceleration(2, 0);
-
-	velocity[0][0] += a_x * DT;
-	velocity[1][0] += a_y * DT;
-	velocity[2][0] += a_z * DT;
+	Vector3d velocity(std::array<double, 3>({
+		speed_m_s * std::cos(pitch) * std::cos(yaw),
+		speed_m_s * std::cos(pitch) * std::sin(yaw),
+		speed_m_s * std::sin(pitch)
+	}));
 
 	return velocity;
 }
