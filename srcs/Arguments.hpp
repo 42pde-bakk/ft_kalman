8d53f9d163c082c6ad4bff26aba688d7a1f81009
Alpha1337k <oscar@kruithof.io>
2024-12-16 16:02:16 +0100

    Fix: IOT Dump + cleanup

--- srcs/Arguments.hpp
+++ srcs/Arguments.hpp
@@ -1,5 +1,5 @@
-#ifndef ARUGMENTS_HPP
-#define ARUGMENTS_HPP
+#ifndef ARGUMENTS_HPP
+#define ARGUMENTS_HPP
 
 #include <cassert>
 #include <string>
@@ -14,8 +14,8 @@ enum DebugLevel {
 class Arguments
 {
 	static char *get_next_argument(size_t &idx, const size_t argc, char **argv) {
-		if (idx >= argc) {
-			std::cerr << "ft_kalman: error: " << argv[idx] << " requires a argument" << std::endl;
+		if (idx + 1 >= argc) {
+			std::cerr << "ft_kalman: error: " << argv[idx] << " requires an argument" << std::endl;
 			exit(1);
 		}
 		return argv[++idx];
@@ -26,27 +26,25 @@ public:
 	DebugLevel debug = DebugLevel::None;
 
 	Arguments(const int argc, char **argv) {
-		for (size_t i = 0; i < static_cast<size_t>(argc); i++)
+		for (size_t i = 1; i < static_cast<size_t>(argc); i++)
 		{
 			if (auto item = std::string(argv[i]); item == "-p" || item == "--port") {
 				this->port = std::stoi(get_next_argument(i, argc, argv));
-				assert(this->port > 0 && this->port < 16384);
-			}
 
-			else if (item == "-i" || item == "--iterations") {
+				assert(this->port > 1023 && this->port < 16384);
+			} else if (item == "-i" || item == "--iterations") {
 				this->iter_limit = std::stoi(get_next_argument(i, argc, argv));
+
 				assert(this->iter_limit > 0);
-			}
-			else if (item == "-v") {
-				this->debug = DebugLevel::Enabled;
-			} else if (item == "-vv") {
-				this->debug = DebugLevel::Verbose;
 			} else if (item == "--help") {
 				std::cout << "usage: ft_kalman [ARGS]" << '\n';
 				std::cout << "\t-p       port used for connecting to the stream" << '\n';
 				std::cout << "\t-i       max iterations done by the program" << '\n';
 				std::cout << "\t-v/vv    debug level" << '\n';
 				exit(0);
+			} else {
+				std::cerr << "ft_kalman: error: '" << item << "' is not a valid argument" << std::endl;
+				exit(1);
 			}
 		}
 		
